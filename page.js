28787fb55d345a650b4050eac8ff2940fa06e03d
BrianWard363 <112121502+BrianWard363@users.noreply.github.com>
2023-05-09 09:49:44 -0600

    add async to header and footer

--- page.js
+++ page.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-    // Use $.get() to load the header and footer files asynchronously
-    var headerRequest = $.get('header.html');
-    var footerRequest = $.get('footer.html');
-    // Use $.when() to wait for both requests to complete
-    $.when(headerRequest, footerRequest).done(function(header, footer) {
-      // Append the header and footer to the page
-      $('#header').html(header[0]);
-      $('#footer').html(footer[0]);
-    });
+  // Use $.get() to load the header and footer files asynchronously with the async attribute
+  var headerRequest = $.get('header.html', { async: true });
+  var footerRequest = $.get('footer.html', { async: true });
+  // Use $.when() to wait for both requests to complete
+  $.when(headerRequest, footerRequest).done(function(header, footer) {
+    // Append the header and footer to the page
+    $('#header').html(header[0]);
+    $('#footer').html(footer[0]);
   });
-  
+});
