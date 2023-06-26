1a265ae00f712658cead79afe4597b809088fb60
BrianWard363 <112121502+BrianWard363@users.noreply.github.com>
2023-06-26 17:00:41 -0600

    made forms better and polished how they work

--- forms.js
+++ forms.js
@@ -1,14 +1,53 @@
 function submitForm(event) {
   event.preventDefault(); // Prevent form submission
 
-  var form = event.target; // Get the form element
-  var formId = form.id; // Get the form ID
+  const form = event.target;
+  const formData = new FormData(form);
+  const url = 'https://formsubmit.co/d9a50db1ba50c5ce9ac9c61ab6bf414a';
 
-  // Submit the form
-  form.submit();
+  fetch(url, {
+    method: 'POST',
+    body: formData
+  });
 
-  // Clear the form fields after a delay of 1 second (1000 milliseconds)
-  setTimeout(function() {
-    form.reset();
-  }, 1000);
+  return false;
 }
+
+document.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Perform actions based on form ID
+  if (event.target.id === 'groups-form') {
+    // Actions for groups-form
+    // ...
+  } else if (event.target.id === 'quit-application-form') {
+    // Actions for quit-application-form
+  } else if (event.target.id === 'application-form') {
+    // Actions for application-form
+    // ...
+  }
+
+  // Reset the form (optional)
+  event.target.reset();
+
+  // Display the success message
+  var successMessage = document.getElementById('message');
+  successMessage.style.display = 'flex'; // Change display to flex for centering the content
+});
+
+// Update the showPopup function
+function showPopup() {
+  var popup = document.getElementById('message');
+  popup.style.display = 'flex'; // Change display to flex for centering the content
+}
+
+// Update the closePopup function
+function closePopup() {
+  var popup = document.getElementById('message');
+  popup.style.display = 'none';
+}
+
+// Example usage:
+// Call the closePopup function when the close button is clicked
+document.getElementById('close-button').addEventListener('click', closePopup);
+
